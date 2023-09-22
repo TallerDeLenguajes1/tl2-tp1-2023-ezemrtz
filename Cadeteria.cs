10d5c34c7c96b2ecca15eaeb4a8c21046ca332a3
“Ezequiel <ezequielmartinez2424@gmail.com>
2023-09-21 23:04:05 -0300

    a

--- Cadeteria.cs
+++ Cadeteria.cs
@@ -81,42 +81,5 @@ public int CantidadPedidos(){
         return cant;
 
     }
-    public void Informe(){
-        float montoTotal = 0;
-        int cantPedidosEnvTotal = 0;
-        foreach (var cad in listadoCadetes)
-        {   
-            float monto = 0;
-            int cantPedidos = 0;
-            int cantPedidosEnviados = 0;
-            Console.WriteLine("=============");
-            Console.WriteLine("ID: {0}", cad.Id);
-            Console.WriteLine("Nombre: {0}", cad.Nombre);
-            foreach (var ped in listadoPedidos)
-            {
-                if(ped.IdCadete == cad.Id){
-                    cantPedidos++;
-                    if(ped.Estado == Estados.entregado){
-                        cantPedidosEnviados++;
-                        cantPedidosEnvTotal++;
-                        monto += 500;
-                    }
-                }
-            }
-            Console.WriteLine("Cantidad pedidos: {0}", cantPedidos);
-            Console.WriteLine("Cantidad pedidos enviados: {0}", cantPedidosEnviados);
-            Console.WriteLine("Jornal: ${0}", monto);
-            Console.WriteLine("=============");
-            montoTotal += monto;
-        }
-        Console.WriteLine("Monto total recaudado: ${0}", montoTotal);
-        float promedio = 0;
-        if(listadoCadetes.Count != 0){
-            promedio = (float)cantPedidosEnvTotal/listadoCadetes.Count;
-        }
-        Console.WriteLine("Promedio de envios por cadete: {0}", promedio);
-    }
-
-
     
 }
